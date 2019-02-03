9665d6040ce9393af5c41c327d8eebd9219cde52
Jeremiah Norris <jjno91@gmail.com>
2019-02-03 15:28:19 -0600

    correct output typo

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "sg_id" {
-  value       = "${aws_security_group.this.id}"
+  value = "${aws_security_group.this.id}"
 }
 
 output "alb_arn" {
-  value       = ${aws_lb.this.arn}
+  value = "${aws_lb.this.arn}"
 }
 
 output "listener_arn" {
-  value       = "${aws_lb_listener.https.arn}"
+  value = "${aws_lb_listener.https.arn}"
 }
